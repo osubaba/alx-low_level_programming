24295c651960c2ec1009c867dfa750696fe7d3b1
osubaba <austineochij@gmail.com>
2023-06-15 15:59:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  *
- * Return : Always returns (O)
+ * Return: Always returns 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art");
 
 	return (0);
 }
+
