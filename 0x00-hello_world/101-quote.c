40f5dbac82f5e61dd359dfaa29fe01bdb43b86cb
osubaba <austineochij@gmail.com>
2023-06-19 15:07:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- *
- * Return : Always returns 0 (succes)
- */
+*main - Entry point
+*Discription : piece of art
+*Return: Always returns 1 (succes)
+*/
 int main(void)
-{
-	write(2 "and that piece of art is useful" - Dora Korpar, 2015-10-19,)
-
-	return (1);
+{	
+write(2,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
 
