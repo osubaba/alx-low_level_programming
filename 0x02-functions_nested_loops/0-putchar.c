5222808e47a0f9fc73399725e37fe8d903b82a3c
osubaba <austineochij@gmail.com>
2023-06-20 07:56:12 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <main.h>
+/**
+ * main - main block
+ * Discription : using putchar
+ * Return: 0(success)
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+	int _putchar(char c);
+{
+	return (write(1, &c, 1));
+}
+
+
