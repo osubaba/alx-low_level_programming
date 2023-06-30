db2438fe28b07c2e1716e6949dcfb2dc5b194b07
osubaba <austineochij@gmail.com>
2023-06-29 18:13:02 -0700

    osu

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,32 @@
 #include "main.h"
-
 /**
- * leet - encode a string into 1337
- * @s: string
- * Return: encoded string `s`
- */
-
+*leet - encoded string
+*Letters a and A should be replaced by 4
+*Letters e and E should be replaced by 3
+*Letters o and O should be replaced by 0
+*Letters t and T should be replaced by 7
+*Letters l and L should be replaced by 1
+*@s: string source
+*Return: string
+*/
 char *leet(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
-	}
-
-	return (s);
+int v, r;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+v = 0;
+while (s[v] != '\0')
+{
+r = 0;
+while (r < 10)
+{
+if (a[r] == s[v])
+{
+s[v] = b[r];
+}
+r++;
+}
+v++;
+}
+return (s);
 }
