15590083357b9a014fb6b6668c01713e5bc443f4
osubaba <austineochij@gmail.com>
2023-07-09 13:52:54 -0700

    mv mynameis mynewnameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,18 +1,15 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * main - print the name of the file followed by a new line
- * @argc: argument count
- * @argv: arguments passed to main as an array of strings
- * Return: 0
+ * main - prints the new name 
+ * @argc: counts arguments to main
+ * @argv: an array of strings
+ * Return: (0) success
  */
-
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char **argv)
 {
-	(void) argc;
+	printf("%s\n", *argv);
 
-	printf("%s\n", argv[0]);
+	return(0);
 
-	return (0);
 }
