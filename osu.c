16da12d9a2604a7ba7da140e8c18082346c9229d
osubaba <austineochij@gmail.com>
2023-07-21 04:58:28 -0700

    Fix bug in strcat function

---
+++ osu.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Discription - using printf and scanf
+ * Return : returns 0
+ */
+int main(void)
+{
+	char name[50] = "osubaba";
+
+	puts(name);
+
+	return (0);
+}
---
+++ putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description : putchar functions
+ * Returns : 0 (succes)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'A'; ch = 'Z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
