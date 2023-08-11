af867bb33e00a9f58ef1acf237730e638778b578
osubaba <austineochij@gmail.com>
2023-08-11 10:34:00 -0700

    iofile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,53 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
 
 /**
- * read_textfile - Reads and prints a text file to standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed.
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, num_read, num_written;
+	int fd;
+	ssize_t rcount, wcount;
 	char *buffer;
 
 	if (filename == NULL)
-		return 0;
+		return (0);
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
-		return 0;
-
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
 	{
 		free(buffer);
-		return 0;
+		return (0);
 	}
+	rcount = read(fd, buffer, letters);
+	if (rcount == -1)
+		return (0);
 
-	num_read = read(fd, buffer, letters);
-	if (num_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return 0;
-	}
-
-	num_written = write(STDOUT_FILENO, buffer, num_read);
-	if (num_written == -1 || num_written != num_read)
-	{
-		free(buffer);
-		close(fd);
-		return 0;
-	}
-
+	wcount = write(STDOUT_FILENO, buffer, rcount);
+	if (wcount == -1 || rcount != wcount)
+		return (0);
 	free(buffer);
-	close(fd);
 
-	return num_written;
+	close(fd);
+	return (wcount);
 }
